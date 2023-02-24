5608d4e9694d1ff94eb3296fff6ef1fd26ee2e70
anonym <0xanonym@proton.me>
2023-02-24 22:53:54 +0530

    File encryption

--- android/app/src/main/kotlin/xmr/anon_wallet/wallet/utils/EncryptUtil.kt
+++ android/app/src/main/kotlin/xmr/anon_wallet/wallet/utils/EncryptUtil.kt
@@ -3,16 +3,21 @@ package xmr.anon_wallet.wallet.utils
 import org.bouncycastle.crypto.digests.SHA256Digest
 import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator
 import org.bouncycastle.crypto.params.KeyParameter
+import org.bouncycastle.util.encoders.Base64
+import java.io.*
 import java.security.SecureRandom
 import javax.crypto.Cipher
+import javax.crypto.CipherInputStream
+import javax.crypto.CipherOutputStream
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
-import org.bouncycastle.util.encoders.Base64
 
 object EncryptUtil {
 
-    const val DEFAULT_ITR = 10000
+    const val DEFAULT_ITR = 11000
     const val SALT_PREFIX = "Salted__"
+    const val BUFFER_SIZE = 4096
+
 
     fun encrypt(
         password: String,
@@ -71,6 +76,63 @@ object EncryptUtil {
         val plaintext = decrypted.copyOfRange(0, decrypted.size - decrypted.last().toInt())
         return plaintext.toString(Charsets.UTF_8)
 
+    }
+
+    fun encryptFile(key: String, inputFile: File, outputFile: File) {
+        val salt = SecureRandom().generateSeed(8)
+        // Derive 48 byte key
+        var keySpec: SecretKeySpec?;
+        var ivSpec: IvParameterSpec?;
+        PKCS5S2ParametersGenerator(SHA256Digest()).let { generator ->
+            generator.init(key.toByteArray(), salt, DEFAULT_ITR)
+            (generator.generateDerivedMacParameters(48 * 8) as KeyParameter).key.let { secretKey ->
+                val bytes = secretKey.copyOfRange(0, 32);
+                keySpec = SecretKeySpec(bytes, "AES")
+                ivSpec = IvParameterSpec(secretKey.copyOfRange(32, secretKey.size))
+                generator.password.fill('*'.code.toByte())
+                secretKey.fill('*'.code.toByte())
+            }
+        }
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec)
+        val fileInputStream = FileInputStream(inputFile)
+        val fileOutputStream = FileOutputStream(outputFile);
+        val cipherOutputStream = CipherOutputStream(BufferedOutputStream(fileOutputStream), cipher)
+        fileOutputStream.write((SALT_PREFIX.toByteArray() + salt))
+        fileInputStream.copyTo(cipherOutputStream, BUFFER_SIZE)
+        fileInputStream.close()
+        cipherOutputStream.close()
+
+    }
+
+    fun decryptFile(key: String, inputFile: File, outputFile: File) {
+        val fis = FileInputStream(inputFile)
+        val prefix = ByteArray(16)
+        fis.read(prefix)
+        //Extract salt from the input file, 0-8 is the salt prefix, 8-16 is the salt
+        val salt = prefix.copyOfRange(8, 16)
+        var keySpec: SecretKeySpec?
+        var ivSpec: IvParameterSpec?
+        PKCS5S2ParametersGenerator(SHA256Digest()).let { generator ->
+            generator.init(key.toByteArray(), salt, DEFAULT_ITR)
+            (generator.generateDerivedMacParameters(48 * 8) as KeyParameter).key.let { secretKey ->
+                val bytes = secretKey.copyOfRange(0, 32);
+                keySpec = SecretKeySpec(bytes, "AES")
+                ivSpec = IvParameterSpec(secretKey.copyOfRange(32, secretKey.size))
+                generator.password.fill('*'.code.toByte())
+                secretKey.fill('*'.code.toByte())
+            }
+        }
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
+        val fileInputStream = inputFile.inputStream()
+        // Skip the salt prefix and salt
+        fileInputStream.skip(16)
+        val cis = CipherInputStream(BufferedInputStream(fileInputStream), cipher)
+        val fos = outputFile.outputStream()
+        cis.copyTo(fos, BUFFER_SIZE)
+        cis.close()
+        fos.close()
     }
 
 }
